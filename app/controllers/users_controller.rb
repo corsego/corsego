83d539133b819ad759a7da71b3cfc9d2e767dc7e
Yaro <yshmarov@gmail.com>
2020-04-07 09:25:26 +0000

    gem ransack

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order(created_at: :desc)
+    #@users = User.all.order(created_at: :desc)
+
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
   end
 end
