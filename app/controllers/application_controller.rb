ff48a029d0a0f4f5ad0e001e89fe1362d09952a3
Yaro <yshmarov@gmail.com>
2020-04-07 13:55:57 +0000

    gem ransack for search in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  before_action :set_global_variables, if: :user_signed_in?
+  def set_global_variables
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
+  end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,13 +2,16 @@ class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
 
   def index
-    if params[:title]
-      @courses = Course.where('title ILIKE ?', "%#{params[:title]}%") #case-insensitive
-    else
-      #@courses = Course.all
-      @q = Course.ransack(params[:q])
-      @courses = @q.result.includes(:user)
-    end
+    #if params[:title]
+    #  @courses = Course.where('title ILIKE ?', "%#{params[:title]}%") #case-insensitive
+    #else
+    #  #@courses = Course.all
+    #  
+    #  #@q = Course.ransack(params[:q])
+    #  #@courses = @q.result.includes(:user)
+    #end
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+    @courses = @ransack_courses.result.includes(:user)
   end
 
   def show
