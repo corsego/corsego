ef1182f294780fe9b64179113a84cc7118f3f789
Yaro <yshmarov@gmail.com>
2020-04-07 20:27:41 +0000

    gem pundit - authorization for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -28,6 +28,7 @@ class CoursesController < ApplicationController
 
   def new
     @course = Course.new
+    authorize @course
   end
 
   def edit
@@ -36,6 +37,7 @@ class CoursesController < ApplicationController
 
   def create
     @course = Course.new(course_params)
+    authorize @course
     @course.user = current_user
 
     respond_to do |format|
@@ -50,6 +52,7 @@ class CoursesController < ApplicationController
   end
 
   def update
+    authorize @course
     respond_to do |format|
       if @course.update(course_params)
         format.html { redirect_to @course, notice: 'Course was successfully updated.' }
@@ -62,6 +65,7 @@ class CoursesController < ApplicationController
   end
 
   def destroy
+    authorize @course
     @course.destroy
     respond_to do |format|
       format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -6,6 +6,22 @@ class CoursePolicy < ApplicationPolicy
   end
   
   def edit?
-    @user.has_role?:admin
+    @user.has_role?:admin || @record.user = @user
+  end
+
+  def update?
+    @user.has_role?:admin || @record.user = @user
+  end
+
+  def new?
+    @user.has_role?:teacher
+  end
+
+  def create?
+    @user.has_role?:teacher
+  end
+
+  def destroy?
+    @user.has_role?:admin || @record.user = @user
   end
 end
