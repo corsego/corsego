0a472d59430095f8299d70143f467843fc853b56
Yaro <yshmarov@gmail.com>
2020-04-07 18:27:07 +0000

    gem rolify - edit roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,32 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def index
     #@users = User.all.order(created_at: :desc)
 
     @q = User.ransack(params[:q])
     @users = @q.result(distinct: true)
   end
+
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User roles were successfully updated.'
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit({role_ids: []})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :show, :update]
   get 'home/index'
   get 'home/activity'
   root 'home#index'
