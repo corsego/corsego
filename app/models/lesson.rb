9ef0ce68da9ed827645d58b53e8697e970080254
Yaro <yshmarov@gmail.com>
2020-04-08 12:41:44 +0000

    public_activity for Lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,11 @@ class Lesson < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
+  def to_s
+    title
+  end
+
 end
