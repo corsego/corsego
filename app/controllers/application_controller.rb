7813499543de316e86d678b26c4d3fd8a1d26e61
Yaro <yshmarov@gmail.com>
2020-04-08 12:03:09 +0000

    check if current_user is online

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery
 
+  after_action :user_activity
+
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -13,6 +15,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def user_activity
+    current_user.try :touch
+  end
 
   def user_not_authorized #pundit
     flash[:alert] = "You are not authorized to perform this action."
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,10 @@ class User < ApplicationRecord
 
   validate :must_have_a_role, on: :update
 
+  def online?
+    updated_at > 2.minutes.ago
+  end
+
   private
   def must_have_a_role
     unless roles.any?
