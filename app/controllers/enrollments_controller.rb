6ec8534667d41b747208d47031f3a5c8cfbff67f
Yaro <yshmarov@gmail.com>
2020-04-11 00:27:15 +0000

    gem ransack for enrollments index

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -4,7 +4,10 @@ class EnrollmentsController < ApplicationController
 
   def index
     #@enrollments = Enrollment.all
-    @pagy, @enrollments = pagy(Enrollment.all)
+    #@pagy, @enrollments = pagy(Enrollment.all)
+
+    @q = Enrollment.ransack(params[:q])
+    @pagy, @enrollments = pagy(@q.result.includes(:user))
 
     authorize @enrollments
   end
