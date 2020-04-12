e43d600d371f81d9ba1fb691b0ce23bd2ded0fb3
Yaro <yshmarov@gmail.com>
2020-04-12 01:37:51 +0000

    Dynamic & Personalized Homepage (Popular, New, Top rated Courses)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   def index
     @courses = Course.all.limit(3)
     @latest_couses = Course.all.limit(3).order(created_at: :desc)
+    @latest_good_reviews = Enrollment.reviewed.order(rating: :desc, created_at: :desc).limit(3)
+    @top_rated_courses = Course.order(average_rating: :desc, created_at: :desc).limit(3)
+    @popular_courses = Course.order(enrollments_count: :desc, created_at: :desc).limit(3)
+    @purchased_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).order(created_at: :desc).limit(3)
   end
   
   def activity
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -15,6 +15,7 @@ class Enrollment < ApplicationRecord
   validate :cant_subscribe_to_own_course  #user can't create a subscription if course.user == current_user.id
 
   scope :pending_review, -> { where(rating: [0, nil, ""], review: [0, nil, ""]) }
+  scope :reviewed, -> { where.not(review: [0, nil, ""]) }
 
   extend FriendlyId
   friendly_id :to_s, use: :slugged
