4d534c48d525d4295edb8245c10bffbd438646af
Yaro <yshmarov@gmail.com>
2020-04-18 20:34:03 +0000

    add recaptcha keys to credentials

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,7 @@
 # config/initializers/recaptcha.rb
 Recaptcha.configure do |config|
-  config.site_key  = '6Le09eoUAAAAAL-oQMopCXt4ZQ0mmue3OCa-i-ZZ'
-  config.secret_key = '6Le09eoUAAAAABYyCfE0fcK6y67q7VRWwcbjIXGq'
+  config.site_key  = Rails.application.credentials.dig(:recaptcha, :site_key)
+  config.secret_key = Rails.application.credentials.dig(:recaptcha, :secret_key)
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
