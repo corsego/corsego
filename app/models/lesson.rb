3e129fd97cf80ef4202ab482d5a7e653c4650b00
Yaro <yshmarov@gmail.com>
2020-04-20 15:33:47 +0000

    video attachment and preview with active_storage and HTML5

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,9 +4,18 @@ class Lesson < ApplicationRecord
   has_many :user_lessons, dependent: :destroy
 
   validates :title, :content, :course, presence: true
-  validates :title, uniqueness: true, length: { maximum: 70 }
+  validates :title, length: { maximum: 70 }
+  validates_uniqueness_of :title, scope: :course_id
 
   has_rich_text :content
+  has_one_attached :video
+  has_one_attached :video_thumbnail
+  validates :video,
+    content_type: ['video/mp4'], 
+    size: { less_than: 50.megabytes , message: 'size should be under 50 megabytes' }
+  validates :video_thumbnail,
+    content_type: ['image/png', 'image/jpg', 'image/jpeg'], 
+    size: { less_than: 500.kilobytes , message: 'size should be under 500 kilobytes' }
 
   extend FriendlyId
   friendly_id :title, use: :slugged
