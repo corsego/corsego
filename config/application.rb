8d769fa2407052e7e6acf3479820df326543a5d3
Yaro <yshmarov@gmail.com>
2020-04-20 18:02:27 +0000

    action_text - play audio with HTML5

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Corsego
       ActionText::ContentHelper.allowed_attributes.add 'controls'
     
       ActionText::ContentHelper.allowed_tags.add 'video'
+      ActionText::ContentHelper.allowed_tags.add 'audio'
       ActionText::ContentHelper.allowed_tags.add 'source'
     end
 
