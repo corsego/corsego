db89448009a5904e0c537baccd3939f838b03b5d
Yaro <yshmarov@gmail.com>
2020-05-05 18:39:57 +0000

    gem wicked - edit and update functionality

--- app/controllers/courses/course_wizard_controller.rb
+++ app/controllers/courses/course_wizard_controller.rb
@@ -1,22 +1,33 @@
 class Courses::CourseWizardController < ApplicationController
   include Wicked::Wizard
-  before_action :set_progress, only: [:show]
+  before_action :set_progress, only: [:show, :update]
+  before_action :set_course, only: [:show, :update, :finish_wizard_path]
 
   steps :basic_info, :details
 
   def show
-    @course = Course.friendly.find params[:course_id]
     #@user = current_user
-    #case step
-    #when :find_friends
-    #  @friends = @user.find_friends
-    #end
+    case step
+    when :basic_info
+    when :details
+      @tags = Tag.all
+    end
     render_wizard
+  end
+
+  def update
+    case step
+    when :basic_info
+      @course.update_attributes(course_params)
+    when :details
+      @tags = Tag.all
+      @course.update_attributes(course_params)
+    end
+    render_wizard @course
   end
 
   def finish_wizard_path
     #courses_path
-    @course = Course.friendly.find params[:course_id]
     course_path(@course)
   end
   
@@ -27,6 +38,15 @@ class Courses::CourseWizardController < ApplicationController
       else
         @progress = 0
       end
+    end
+
+    def set_course
+      @course = Course.friendly.find params[:course_id]
+    end
+
+    def course_params
+      params.require(:course).permit(:title, :description, :short_description, :price,
+        :published, :language, :level, :avatar, tag_ids: [])
     end
 
 end
