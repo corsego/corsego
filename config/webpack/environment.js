b42d5306290bca8b4fc9fac187c198b46167d37c
Yaro <yshmarov@gmail.com>
2020-05-31 21:44:39 +0000

    change meta tags (no video)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,6 +3,7 @@ const webpack = require("webpack")
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
 $: 'jquery',
 jQuery: 'jquery',
+//'window.jQuery': 'jquery',
 Popper: ['popper.js', 'default']
 }))
 module.exports = environment
