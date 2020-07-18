ef1f251abcb093a7a843be9b9b110efb1a2fa427
Yaro <yshmarov@gmail.com>
2020-07-18 09:45:17 +0000

    UserMailer.new_user works for omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,43 +1,42 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-
-      if @user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect @user, event: :authentication
-      else
-        session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-      end
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      UserMailer.new_user(@user).deliver_later
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
   end
 
   def github
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-
-      if @user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Github'
-        sign_in_and_redirect @user, event: :authentication
-      else
-        session['devise.github_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-      end
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      UserMailer.new_user(@user).deliver_later
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Github'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session['devise.github_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
   end
 
   def facebook
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-
-      if @user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Facebook'
-        sign_in_and_redirect @user, event: :authentication
-      else
-        session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-      end
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      UserMailer.new_user(@user).deliver_later
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Facebook'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
   end
 
   def failure
     redirect_to root_path, alert: "Failure. Please try again"
   end
-
 end
