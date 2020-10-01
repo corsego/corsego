ea150c63cb0620e659caf185f11438fe21cd47e9
Yaro <yshmarov@gmail.com>
2020-10-01 10:43:50 +0000

    do not send admin email if user re-logged-in with omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def handle_auth(kind)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      UserMailer.new_user(@user).deliver_later
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: kind
       sign_in_and_redirect @user, event: :authentication
     else
