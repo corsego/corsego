97674eda3a287cba4c9fb473f827be513a191ef2
Yaro <yshmarov@gmail.com>
2020-10-13 12:25:14 +0000

    boolean_label helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,14 @@ module ApplicationHelper
         "<i class='fa fa-comment'></i>".html_safe
     end
   end
+
+  def boolean_label(value)
+    case value
+      when true
+        content_tag(:span, value, class: "badge badge-success")
+      when false
+        content_tag(:span, value, class: "badge badge-danger")
+    end
+  end
+
 end
