61cac7cf2e46577dea79c1631a46bed04eea56e0
Yaro <yshmarov@gmail.com>
2020-11-29 01:20:40 +0000

    better scss order, vimeo 3by2 iframe

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,13 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("stylesheets/application.scss")
-
 import 'bootstrap/dist/js/bootstrap'
 import "bootstrap/dist/css/bootstrap";
 
 import "@fortawesome/fontawesome-free/css/all"
 
+require("stylesheets/application.scss")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
