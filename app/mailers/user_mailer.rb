253819066c7708b0f62d44e89ebcefb6b9d816a0
Yaro <yshmarov@gmail.com>
2021-05-15 20:53:16 +0000

    minor styling

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,5 +2,6 @@ class UserMailer < ApplicationMailer
   def new_user(user)
     @user = user
     mail(to: User.with_role(:admin).distinct.pluck(:email), subject: "Corsego: #{@user.email} registred")
+    # email_address_with_name(@user.email, @user.username)
   end
 end
