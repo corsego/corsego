f16773a17cef76647caf2c5fe5db58ac8d8a2e07
Yaro <yshmarov@gmail.com>
2021-05-16 08:53:04 +0000

    landing page styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,23 +2,15 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:landing_page, :privacy_policy]
 
   def landing_page
-    @latest_good_reviews = Enrollment.reviewed.latest_good_reviews.limit(2)
-    @latest = Course.latest.published.approved.limit(2)
-    @top_rated = Course.top_rated.published.approved.limit(2)
-    @popular = Course.popular.published.approved.limit(2)
-    if current_user
-      @learning_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).order(created_at: :desc).limit(2)
-      @teaching_courses = current_user.courses.limit(2)
-    end
+    @courses = Course.published.approved.order(enrollments_count: :desc, created_at: :desc)
     @popular_tags = Tag.all.where.not(course_tags_count: 0).order(course_tags_count: :desc).limit(10)
   end
 
   def activity
-    if current_user.has_role?(:admin)
-      @pagy, @activities = pagy(PublicActivity::Activity.all.order(created_at: :desc))
-    else
+    unless current_user.has_role?(:admin)
       redirect_to root_path, alert: "You are not authorized to access this page"
     end
+    @pagy, @activities = pagy(PublicActivity::Activity.all.order(created_at: :desc))
   end
 
   def analytics
