f2f25b6cad9c6c278be82f09eb332e2845a086a0
Yaro <yshmarov@gmail.com>
2021-05-22 14:33:27 +0000

    set defaults to rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Corsego
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     if Rails.env.development? # for rails-erd gem to generate a diagram
       def eager_load!
